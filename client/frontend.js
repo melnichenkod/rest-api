6aa197b8164b6e0ee39f9938b691f52e7db37d0d
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-11-08 17:12:30 +0200

    added the ability to create a card

--- client/frontend.js
+++ client/frontend.js
@@ -1,1 +1,22 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data(){
+    return {
+      form: {
+        name: '',
+        value: ''
+      },
+      contacts: []
+    }
+  },
+  methods: {
+    createContact(){
+      const {...contact} = this.form;
+      //console.log(contact);
+      this.contacts.push({...contact, id: Date.now()})
+      this.form.name = this.form.value = ''
+    }
+  }
+})
