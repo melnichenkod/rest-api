1b1e0d829cd1ce2da9b230b6c7c42e21074040bc
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-11-08 20:16:05 +0200

    added the ability to mark and to remove contact

--- client/frontend.js
+++ client/frontend.js
@@ -11,12 +11,24 @@ new Vue({
       contacts: []
     }
   },
+  computed: {
+    canCreate(){
+      return this.form.value.trim() && this.form.name.trim()
+    }
+  },
   methods: {
     createContact(){
       const {...contact} = this.form;
       //console.log(contact);
-      this.contacts.push({...contact, id: Date.now()})
+      this.contacts.push({...contact, id: Date.now(), marked: false})
       this.form.name = this.form.value = ''
+    },
+    markContact(id){
+      const contact = this.contacts.find(item => item.id === id);
+      contact.marked = true;
+    },
+    removeContact(id){
+      this.contacts = this.contacts.filter(item => item.id !== id)
     }
   }
 })
